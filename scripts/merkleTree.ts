df939c58d4822b8e27fc0ebb9b6a284b58cb0d9c
joaovwfreire <jvwfreire@gmail.com>
2023-05-17 23:19:08 -0300

    merkletree generator from pending wtd at db

--- scripts/merkleTree.ts
+++ scripts/merkleTree.ts
@@ -1,20 +1,49 @@
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
+const mysql = require('mysql2')
 
+require('dotenv').config()
 // Utilizes https://github.com/OpenZeppelin/merkle-tree to generate Merkle Trees.
+
+type PendingWithdrawal = {
+  address: string,
+  token_address: string,
+  amount: string,
+}
+
+const fetchPendingWithdrawals = async () => {
+  const connection = mysql.createConnection(process.env.DATABASE_URL)
+  const [rows, fields] = await connection.promise().query('SELECT * FROM pending_withdrawals')
+  connection.end()
+  return rows
+}
+
+
 
 const main = async () => {
 
-    const values = [
-        ["0x1111111111111111111111111111111111111111", "5000000000000000000"],
-        ["0x2222222222222222222222222222222222222222", "2500000000000000000"]
-      ];
+  let values: any[] = []
 
-    const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+  const connection = mysql.createConnection(process.env.DATABASE_URL)
+  console.log('Connected to PlanetScale!')
+  connection.end()
 
+  const pendingWithdrawals = await fetchPendingWithdrawals()
+  console.log(pendingWithdrawals)
+
+  for (let i = 0; i < pendingWithdrawals.length; i++) {
+    if(pendingWithdrawals[i].pending == 1){
+      const value = [pendingWithdrawals[i].address, pendingWithdrawals[i].token_address, pendingWithdrawals[i].amount]
+    
+      values.push(value)
+    }
+  }
+    const tree = StandardMerkleTree.of(values, ["address", "address", "uint256"]);
+
+    console.log(values)
     const root = tree.root;
     const proofZero = tree.getProof(0);
     const proofOne = tree.getProof(1);
-    
+  /*  
     const verified = StandardMerkleTree.verify(root, ['address', 'uint'], values[0], proofZero);
     console.log(verified); // TRUE
 
@@ -23,7 +52,7 @@ const main = async () => {
 
     const verified3 = StandardMerkleTree.verify(root, ['address', 'uint'], values[0], proofOne);
     console.log(verified3); // FALSE
-
+*/
     console.log(tree);
 
     console.log(tree.render())
